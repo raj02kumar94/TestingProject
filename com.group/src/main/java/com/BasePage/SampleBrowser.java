2a76964e9e22a9de63498faae579755cb283d9ae
raj02kumar94 <166906307+raj02kumar94@users.noreply.github.com>
2024-04-19 17:18:23 +0530

    New Package

---
+++ com.group/src/main/java/com/BasePage/SampleBrowser.java
@@ -0,0 +1,11 @@
+package com.BasePage;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class SampleBrowser {
+
+	
+	WebDriver driver = new ChromeDriver();
+	
+	
+}
