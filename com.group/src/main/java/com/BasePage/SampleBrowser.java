179792661e7845143104187810aee4df39323246
raj02kumar94 <166906307+raj02kumar94@users.noreply.github.com>
2024-04-19 17:35:01 +0530

    New Package

--- com.group/src/main/java/com/BasePage/SampleBrowser.java
+++ com.group/src/main/java/com/BasePage/SampleBrowser.java
@@ -7,5 +7,11 @@ public class SampleBrowser {
 	
 	WebDriver driver = new ChromeDriver();
 	
+
 	
+	public static void mainluanchBro(String[] args)
+	{
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pavithra\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe");
+	}
 }
